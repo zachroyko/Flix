4cb7b2827f28f31f77a060d9c29693939e45a7fb
Zach <zach@pal-nat186-48-180.itap.purdue.edu>
2019-01-14 18:21:04 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Zach on 1/14/19.
+//  Copyright © 2019 Zach Royko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
