31e7a5ad42edb0e97d0cc25c010c9a54a157807d
Zach <zach@zachs-mbp.local>
2019-01-27 19:44:50 -0500

    Added superhero tab bar

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Zach on 1/27/19.
+//  Copyright © 2019 Zach Royko. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
